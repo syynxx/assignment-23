69add542b4edb21a0e7bece35fdbeb7d5fa7748e
syynxx <116304500+syynxx@users.noreply.github.com>
2022-12-19 14:30:00 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+//function defintion
+function hello(name) {
+    alert("hello " + name);
+}
+
+//calls
+hello("can you find the secret message");
+
+function secretMessage(message) {
+    console.log("The secret message is " + message);
+}
+//calls
+secretMessage("dogs"); 
+secretMessage("dogs"); 
+secretMessage("dogs"); 
+secretMessage("dogs"); 
